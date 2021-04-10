44ca6df43d45c19afe1d11adfb0c355378c74b07
yam <yamlakyam@gmail.com>
2021-04-10 11:09:43 +0300

    made mutiple buttons to correspond to key notes

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+
 //import 'package:english_words/english_words.dart';
 //import 'package:audioplayers/audioplayers.dart';
 import 'package:audioplayers/audio_cache.dart';
 
-
-
 void main() {
   runApp(MyApp());
+}
+void playNotes(int noteNum){
+  final player = AudioCache();
+  player.play('assets_note$noteNum.wav');
 }
 
 class MyApp extends StatelessWidget {
@@ -15,11 +18,52 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: FlatButton(onPressed: (){
-              final player = AudioCache();
-              player.play('note1.wav');
-            },child:Text('ola') ,),
+          child: Column(
+            children: [
+              FlatButton(
+                color: Colors.blue,
+                onPressed: () {
+                  playNotes(1);
+                },
+                child: Text('ola'),
+              ),FlatButton(
+                color: Colors.blueAccent,
+                onPressed: () {
+                  playNotes(2);
+                },
+                child: Text('ola'),
+              ),FlatButton(
+                color: Colors.blueGrey,
+                onPressed: () {
+                  playNotes(3);
+                },
+                child: Text('ola'),
+              ),FlatButton(
+                color: Colors.lightBlue,
+                onPressed: () {
+                  playNotes(4);
+                },
+                child: Text('ola'),
+              ),FlatButton(
+                color: Colors.lightBlueAccent,
+                onPressed: () {
+                  playNotes(5);
+                },
+                child: Text('ola'),
+              ),FlatButton(
+                color: Colors.deepPurpleAccent,
+                onPressed: () {
+                  playNotes(6);
+                },
+                child: Text('ola'),
+              ), FlatButton(
+                color: Colors.deepPurple,
+                onPressed: () {
+                  playNotes(7);
+                },
+                child: Text('ola'),
+              ),
+            ],
           ),
         ),
       ),
