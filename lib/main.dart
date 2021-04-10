1b7061e8b631a2a8194497a008e984845e1000a3
yam <yamlakyam@gmail.com>
2021-04-10 11:22:32 +0300

    made some modification on the UI

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,8 @@ import 'package:audioplayers/audio_cache.dart';
 void main() {
   runApp(MyApp());
 }
-void playNotes(int noteNum){
+
+void playNotes(int noteNum) {
   final player = AudioCache();
   player.play('assets_note$noteNum.wav');
 }
@@ -19,49 +20,73 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              FlatButton(
-                color: Colors.blue,
-                onPressed: () {
-                  playNotes(1);
-                },
-                child: Text('ola'),
-              ),FlatButton(
-                color: Colors.blueAccent,
-                onPressed: () {
-                  playNotes(2);
-                },
-                child: Text('ola'),
-              ),FlatButton(
-                color: Colors.blueGrey,
-                onPressed: () {
-                  playNotes(3);
-                },
-                child: Text('ola'),
-              ),FlatButton(
-                color: Colors.lightBlue,
-                onPressed: () {
-                  playNotes(4);
-                },
-                child: Text('ola'),
-              ),FlatButton(
-                color: Colors.lightBlueAccent,
-                onPressed: () {
-                  playNotes(5);
-                },
-                child: Text('ola'),
-              ),FlatButton(
-                color: Colors.deepPurpleAccent,
-                onPressed: () {
-                  playNotes(6);
-                },
-                child: Text('ola'),
-              ), FlatButton(
-                color: Colors.deepPurple,
-                onPressed: () {
-                  playNotes(7);
-                },
-                child: Text('ola'),
+              Expanded(
+                child: FlatButton(
+                  color: Colors.blue,
+                  onPressed: () {
+                    playNotes(1);
+                  },
+                  child: Text('ola'),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  color: Colors.blueAccent,
+                  onPressed: () {
+                    playNotes(2);
+                  },
+                  child: Text('ola'),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  color: Colors.blueGrey,
+                  onPressed: () {
+                    playNotes(3);
+                  },
+                  child: Text('ola'),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  color: Colors.lightBlue,
+                  onPressed: () {
+                    playNotes(4);
+                  },
+                  child: Text('ola'),
+                ),
+              ),
+
+              Expanded(
+                child: FlatButton(
+                  color: Colors.lightBlueAccent,
+                  onPressed: () {
+                    playNotes(5);
+                  },
+                  child: Text('ola'),
+                ),
+              ),
+
+              Expanded(
+                child: FlatButton(
+                  color: Colors.deepPurpleAccent,
+                  onPressed: () {
+                    playNotes(6);
+                  },
+                  child: Text('ola'),
+                ),
+              ),
+
+              Expanded(
+                child: FlatButton(
+                  color: Colors.deepPurple,
+                  onPressed: () {
+                    playNotes(7);
+                  },
+                  child: Text('ola'),
+                ),
               ),
             ],
           ),
